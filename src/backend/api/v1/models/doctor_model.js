fb1acb6560c68857063b54531381f4d60ad234d0
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-04-25 19:57:54 -0400

    doctor controller, router y model añadido. Carpeta para probar el api creada

---
+++ src/backend/api/v1/models/doctor_model.js
@@ -0,0 +1,87 @@
+const sqlite3 = require('sqlite3').verbose()
+
+// Abre la base de datos en modo lectura/escritura
+const db = new sqlite3.Database('casa_mascota_db.sqlite')
+
+// Crea la tabla "doctores" si no existe
+/*db.run(`CREATE TABLE Doctor (
+   id_doctor integer NOT NULL CONSTRAINT Doctor_pk PRIMARY KEY,
+   nombre varchar(50) NOT NULL,
+   apellido varchar(50) NOT NULL,
+   numero_tel integer NOT NULL,
+   direccion varchar(100) NOT NULL
+)`)*/
+
+// Función para obtener todos los doctores
+exports.getDoctores = (callback) => {
+  db.all('SELECT * FROM Doctor', [], (err, rows) => {
+    if (err) {
+      console.error(err.message)
+      callback(err)
+    } else {
+      callback(null, rows)
+    }
+  })
+}
+
+// Función para obtener un solo doctor por ID
+exports.getDoctorById = (id, callback) => {
+  db.get('SELECT * FROM doctores WHERE id_doctor = ?', [id], (err, row) => {
+    if (err) {
+      console.error(err.message)
+      callback(err)
+    } else {
+      callback(null, row)
+    }
+  })
+}
+
+// Función para crear un nuevo doctor
+exports.createDoctor = (doctor, callback) => {
+  const { nombre, apellido, telefono, direccion } = doctor
+  db.run('INSERT INTO doctores (nombre, apellido, numero_tel, direccion) VALUES (?, ?, ?, ?)', [nombre, apellido, telefono, direccion], (err) => {
+    if (err) {
+      console.error(err.message)
+      callback(err)
+    } else {
+      callback(null)
+    }
+  })
+}
+
+// Función para actualizar un doctor existente
+exports.updateDoctor = (id, doctor, callback) => {
+  const { nombre, apellido, telefono, direccion } = doctor
+  db.run('UPDATE doctores SET nombre = ?, apellido = ?, telefono = ?, direccion = ? WHERE id = ?', [nombre, apellido, telefono, direccion, id], (err) => {
+    if (err) {
+      console.error(err.message)
+      callback(err)
+    } else {
+      callback(null)
+    }
+  })
+}
+
+// Función para eliminar un doctor por ID
+exports.deleteDoctorById = (id, callback) => {
+  db.run('DELETE FROM doctores WHERE id = ?', [id], (err) => {
+    if (err) {
+      console.error(err.message)
+      callback(err)
+    } else {
+      callback(null)
+    }
+  })
+}
+
+// Cierra la conexión a la base de datos cuando se detiene la aplicación
+process.on('SIGINT', () => {
+  db.close((err) => {
+    if (err) {
+      console.error(err.message)
+    } else {
+      console.log('Conexión a la base de datos cerrada')
+    }
+    process.exit(0)
+  })
+})
--- src/backend/server.js
+++ src/backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+const doctorRoute = require('./api/v1/routes/doctor_route');
+app.use('/api/doctores', doctorRoute);
+
 app.get('/', (req, res) => {
   res.send('Hola desde Express.js!');
 });
