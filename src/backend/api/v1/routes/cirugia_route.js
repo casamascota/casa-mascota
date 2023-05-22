fac8f58fc206bd8f8f32a60bdec3682ce4966097
CarlosFULLHD <cninareynaga@gmail.com>
2023-05-21 21:30:28 -0400

    Push

--- src/backend/api/v1/routes/cirugia_route.js
+++ src/backend/api/v1/routes/cirugia_route.js
@@ -1,25 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const cirugiaController = require('../controllers/cirugia_controller');
+const cirugiaController = require("../controllers/cirugia_controller");
 
 // Ruta para obtener todas las cirugias
-router.get('/', cirugiaController.getCirugias);
+router.get("/", cirugiaController.getCirugias);
 // Ruta para obtener todas las cirugias PENDIENTES
-router.get('/', cirugiaController.getCirugiasPendientes);
+router.get("/", cirugiaController.getCirugiasPendientes);
 
 // Ruta para obtener una cirugia por ID
-router.get('/:id', cirugiaController.getCirugiaById);
+router.get("/:id", cirugiaController.getCirugiaById);
 // Ruta para obtener todas las cirugias de un doctor por ID de doctor
-router.get('/doctor/:doctorId', cirugiaController.getCirugiaByDoctorId);
+router.get("/cirugia/:doctorId", cirugiaController.getCirugiaByDoctorId);
 // Ruta para obtener todas las cirugias de un doctor por ID de enfermero
-router.get('/doctor/:doctorId', cirugiaController.getCirugiasByEnfermeroId);
+router.get("/cirugia/:enfermeroId", cirugiaController.getCirugiasByEnfermeroId);
 // Ruta para obtener todas las cirugias de un doctor por ID de diagnostico
-router.get('/doctor/:doctorId', cirugiaController.getCirugiaByDiagnosticoId);
+router.get(
+  "/cirugia/:DiagnosticoId",
+  cirugiaController.getCirugiaByDiagnosticoId
+);
 // Ruta para crear una nueva cirugia
-router.post('/', cirugiaController.createCirugia);
+router.post("/", cirugiaController.createCirugia);
 // Ruta para actualizar una cirugia existente
-router.put('/:id', cirugiaController.updateCirugia);
+router.put("/:id", cirugiaController.updateCirugia);
 // Ruta para eliminar una cirugia por ID
-router.delete('/:id', cirugiaController.deleteCirugiaById);
+router.delete("/:id", cirugiaController.deleteCirugiaById);
 
 module.exports = router;
