42886e423665345b756ad1fb0b9ce2a4606ce0ef
Camila-Negron <carla.negron@ucb.edu.bo>
2023-05-21 20:20:37 -0400

    funcion getOwner

--- src/app/Componentes/Usuario/lista-mascotas/lista-mascotas.component.ts
+++ src/app/Componentes/Usuario/lista-mascotas/lista-mascotas.component.ts
@@ -1,33 +1,65 @@
-import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-mascotas',
   templateUrl: './lista-mascotas.component.html',
   styleUrls: ['./lista-mascotas.component.css']
 })
-export class ListaMascotasComponent {
+export class ListaMascotasComponent implements OnInit {
   URL_BASE = 'http://localhost:3000/api/';
-  mascotas: any[] = [];
+  mascotasList: any[] = [];
 
-  constructor(private httpClient: HttpClient) {
-  this.getAllMascotas();
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getOwner();
   }
- 
 
-  getAllMascotas() {
-    this.httpClient.get(this.URL_BASE + 'mascotas').subscribe(
-       (res: any) => {
-          this.mascotas = res;
-          console.log(this.mascotas);
-       },
-       err => {
-          console.log(err);
-       }
+  _getOwner(): Observable<any> {
+    return this.httpClient.get<any[]>(this.URL_BASE + 'owner');
+  }
+
+  getOwner() {
+    this._getOwner().subscribe(
+      (res) => {
+        const auxLista = res;
+        this.mascotasList = res;
+
+        for (let i = 0; i < auxLista.length; i++) {
+          this.getMascota(auxLista[i].Mascota_id_mascota).subscribe(
+            (res) => {
+              auxLista[i].Mascota_id_mascota = res.nombre;
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
+        }
+
+        for (let i = 0; i < auxLista.length; i++) {
+          this.getOwnerById(auxLista[i].Owner_id_owner).subscribe(
+            (res) => {
+              auxLista[i].Owner_id_owner = res.nombre;
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
     );
- }
+  }
 
- 
+  getMascota(idMascota: number): Observable<any> {
+    return this.httpClient.get(`${this.URL_BASE}mascotas/${idMascota}`);
+  }
 
- 
+  getOwnerById(idOwner: number): Observable<any> {
+    return this.httpClient.get(`${this.URL_BASE}owner/${idOwner}`);
+  }
 }
